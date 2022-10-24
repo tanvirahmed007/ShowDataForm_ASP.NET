682f0bfc39054d3e2891bf90a360bef80345e661
Tanvir <myselftanvirahmedshawon@gmail.com>
2022-10-25 01:14:30 +0600

    Add project files.

---
+++ ShowDataForm/Controllers/CustomerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShowDataForm.Models;
+
+namespace ShowDataForm.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create(CreateCustomer create)
+        {
+            return View(create);
+        }
+    }
+}
---
+++ ShowDataForm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShowDataForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
